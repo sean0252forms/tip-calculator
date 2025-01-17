a04d5d7c5b0e21e397cbed45438384faa83f9a40
sean0252forms <sean0252@gmail.com>
2025-01-17 05:20:45 -0600

    added script.js.txt added functionality

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+let USDollar = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+function convertAmt(price){
+return USDollar.format(price)
+}
 setInterval(function(){
-var output = document.getElementById("output")
+var outputTax = document.getElementById("outputTax")
+var outputTotal = document.getElementById("outputTotal")
 var amount = document.getElementById("amountIn")
 var amountVal = amount.value
 amountVal=Number(amountVal)
 var tax = document.getElementById("tax")
 taxAmt = tax.value
-output.innerHTML=taxAmt/100*amountVal
-
+var taxVal=convertAmt(taxAmt/100*amountVal)
+outputTax.innerHTML=taxVal
+outputTotal.innerText=amountVal+taxVal
 },1)

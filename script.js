af34207efc6a63a38c21d9718abed258f45fa417
sean0252forms <sean0252@gmail.com>
2025-05-14 03:42:36 -0500

    update webpage

--- script.js
+++ script.js
@@ -2,18 +2,39 @@ let USDollar = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
 });
-
+var tax = document.getElementById("tax")
+var outputTax = document.getElementById("outputTax")
+var taxPercent = document.getElementById("taxPercent")
+var outputTotal = document.getElementById("outputTotal")
+var amount = document.getElementById("amountIn")
+var taxValOut = document.getElementById("taxValue")
+var ltdk = document.getElementById('ltdk')
+var textColor
+// function getScreenMode() {
+//   if (ltdk.value=="dark") {
+//     textColor="white"
+//     document.body.style.backgroundColor="black"
+    
+//   } else {
+//     document.body.style.backgroundColor="white"
+//     textColor="black"
+//   }
+//   taxValOut.style.color = textColor
+// taxPercent.style.color = textColor
+// tax.style.color = textColor
+// taxValOut.style.color = textColor
+// taxPercent.style.color = textColor
+// tax.style.color = textColor
+// }
 function convertAmt(price) {
   return USDollar.format(price)
 }
 setInterval(function() {
-  var outputTax = document.getElementById("outputTax")
-  var taxPercent = document.getElementById("taxPercent")
-  var outputTotal = document.getElementById("outputTotal")
-  var amount = document.getElementById("amountIn")
+  // getScreenMode()
+  
   var amountVal = amount.value
   amountVal = Number(amountVal)
-  var tax = document.getElementById("tax")
+  
   taxAmt = tax.value
   var taxVal = taxAmt / 100 * amountVal
   outputTax.innerHTML = convertAmt(taxVal)
